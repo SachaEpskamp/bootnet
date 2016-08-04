b867ef6f4e658479e0c9819ecec69a833536c827
Sacha Epskamp <mail@sachaepskamp.com>
2016-08-04 16:09:33 +0200

    Renamed centralityCompare to differenceTest. Added some functions I forgot to upload...

--- R/centralityCompare.R
+++ R/centralityCompare.R
@@ -1,22 +1,62 @@
-centralityCompare <- function(bootobject,x,y,measure = c("strength","closeness","betweenness")){
+differenceTest <- function(bootobject,x,y,measure = c("strength","closeness","betweenness"),x2,y2){
   
   stopifnot(class(bootobject) == "bootnet")
   
-  if (is.numeric(x)){
-    x <- bootobject$sample$labels[x]
-  }
-  if (is.numeric(y)){
-    y <- bootobject$sample$labels[y]
+  if (any(measure %in% c("strength","betweenness","closeness")) & any(measure %in% c("edge","distance"))){
+    stop("Difference test can not be made for centrality index and edge weights/distances at the same time.")
   }
   
-  cent <- bootobject$bootTable %>% filter(type %in% measure) %>% dplyr::select(name,node1,value,type)
-  fullTable <- expand.grid(name = unique(cent$name),node1=x,node2=y,type = measure,
+  if (!missing(x2)){
+    if (any(measure %in% c("edge","distance"))){
+      opts <- paste0(c(x,x2),"--",c(x2,x))
+      x <- opts[opts%in%bootobject$sampleTable$id][1]
+    } else {
+      warning("'x2' ignored")
+    }
+  }  
+  if (!missing(y2)){
+    if (any(measure %in% c("edge","distance"))){
+      opts <- paste0(c(y,y2),"--",c(y2,y))
+      y <- opts[opts%in%bootobject$sampleTable$id][1]
+    } else {
+      warning("'y2' ignored")
+    }
+  }  
+  
+  if (is.numeric(x)){
+    if (any(measure %in% c("strength","betweenness","closeness"))){
+      x <- bootobject$sample$labels[x]
+    } else {
+      stop("Numeric assignment not possible for edge or distance difference test")
+    }
+  }
+  
+  if (is.numeric(y)){
+    if (any(measure %in% c("strength","betweenness","closeness"))){
+      y <- bootobject$sample$labels[y]
+    } else {
+      stop("Numeric assignment not possible for edge or distance difference test")
+    }
+  }
+ 
+
+  cent <- bootobject$bootTable %>% filter(type %in% measure) %>% dplyr::select(name,id1=id,value,type)
+  
+  if (!all(x %in% cent$id1)){
+    stop("'x' is not a valid ID")
+  }
+  if (!all(y %in% cent$id1)){
+    stop("'y' is not a valid ID")
+  }
+  
+  fullTable <- expand.grid(name = unique(cent$name),id1=x,id2=y,type = measure,
                            stringsAsFactors = FALSE) 
   
+
   Quantiles <- fullTable %>% 
-    left_join(dplyr::select(cent,name,node1,value1=value,type),by=c("name","node1","type")) %>% 
-    left_join(dplyr::select(cent,name,node2=node1,value2=value,type),by=c("name","node2","type"))  %>%
-    group_by(node1,node2,type) %>%
+    left_join(dplyr::select(cent,name,id1=id1,value1=value,type),by=c("name","id1","type")) %>% 
+    left_join(dplyr::select(cent,name,id2=id1,value2=value,type),by=c("name","id2","type"))  %>%
+    group_by(id1,id2,type) %>%
     summarize(lower = quantile(value2-value1,0.025),upper = quantile(value2-value1,0.975)) %>%
     mutate(contain0 = 0 >= lower & 0 <= upper) %>% 
     rename(measure = type) %>% 
@@ -34,98 +74,98 @@ centralityCompare <- function(bootobject,x,y,measure = c("strength","closeness",
   return(Quantiles)
 }
 
-
-overlap <- function(x,measure = c("strength","closeness","betweenness"),
-                    order = c("value","order")){
-  
-  order <- match.arg(order)
-  measure <- match.arg(measure)
-  
-  cent <- x$bootTable %>% filter(type %in% measure) %>% dplyr::select(name,node1,value,type)
-  fullTable <- expand.grid(name = unique(cent$name),node1=unique(cent$node1),node2=unique(cent$node1),type = unique(cent$type),
-                           stringsAsFactors = FALSE) 
-  
-  Quantiles <- fullTable %>% 
-    left_join(dplyr::select(cent,name,node1,value1=value,type),by=c("name","node1","type")) %>% 
-    left_join(dplyr::select(cent,name,node2=node1,value2=value,type),by=c("name","node2","type"))  %>%
-    group_by(node1,node2,type) %>%
-    summarize(lower = quantile(value2-value1,0.025),upper = quantile(value2-value1,0.975)) %>%
-    mutate(contain0 = 0 >= lower & 0 <= upper)
-  
-  #bootmean:
-  bootMeans <- x$bootTable %>% filter(type %in% measure) %>%
-    group_by(node1,type) %>% summarize(mean = mean(value,na.rm=TRUE))
-  
-  sample <- x$sampleTable %>% filter(type %in% measure) %>% dplyr::select(node1,value,type) %>% 
-    left_join(bootMeans,by=c("node1","type"))
-  
-  # Now for every node: minimal node equal to....
-  DF <- Quantiles %>% left_join(dplyr::select(sample,node2=node1,value,type), by = c("node2","type")) %>%
-    group_by(node1,type) %>% 
-    summarize(
-      minNode = node2[contain0][which.min(value[contain0])],
-      maxNode = node2[contain0][which.max(value[contain0])]
-    ) %>% left_join(sample,by=c("node1","type")) %>% ungroup %>%
-    mutate(valueMin = value[match(minNode,node1)], 
-           valueMax = value[match(maxNode,node1)],
-           rank = order(order(value,mean))) %>% arrange(rank)
-  
-  if (order == "value"){
-    levels <- DF$node1[order(DF$value)]  
-  } else  if (order == "order"){
-    levels <- x$sample$labels
-  }
-  
-  Quantiles$node1 <- factor(Quantiles$node1,levels=levels)
-  Quantiles$node2 <- factor(Quantiles$node2,levels=levels)
-  Quantiles$fill <- ifelse(Quantiles$node1 == Quantiles$node2, "same",
-                           ifelse(Quantiles$contain0,"nonsig","sig"))
-  DF$node2 <- DF$node1
-  DF$node1 <- factor(DF$node1,levels=levels)
-  DF$node2 <- factor(DF$node2,levels=levels)
-  DF$label <- as.character(round(DF$value,2))
-  DF$fill <- "same"
-  
-  lab <- measure
-  substr(lab,1,1) <- toupper(substr(lab,1,1))
-  
-  g <- ggplot(Quantiles,aes(x=node1,y=node2,fill=fill)) + 
-    geom_tile(colour = 'white') + xlab("") + ylab("") + 
-    scale_fill_manual(values = c("same" = "white","nonsig" = "lightgray","sig" = "black")) + 
-    geom_text(data=DF,aes(label = label))+ theme(legend.position="none") + 
-    ggtitle(lab)
-  
-  base_size <- 9
-  g <- g + theme_grey(base_size = base_size) + labs(x = "",
-                                                    y = "") + scale_x_discrete(expand = c(0, 0)) +
-    scale_y_discrete(expand = c(0, 0)) + theme(legend.position = "none",
-                                               axis.ticks = element_blank(), axis.text.x = element_text(size = base_size *
-                                                                                                          0.8, angle = 330, hjust = 0, colour = "grey50"))
-  
-  return(g)
-  #   
-  #   plot(DF$value,DF$rank,type="o",xaxt="n",yaxt="n",bty="n",
-  #        xlab = measure, ylab = "node", pch = 17, cex = 2)
-  #   
-  #   for (i in seq_len(nrow(DF))){
-  #     
-  #     lines(c(DF$valueMin[i],DF$valueMax[i]),c(DF$rank[i],DF$rank[i]),lty=2)
-  #     lines(c(DF$valueMin[i],DF$valueMin[i]),c(DF$rank[i],DF$rank[DF$minNode[i]==DF$node1]),lty=2)
-  #     lines(c(DF$valueMax[i],DF$valueMax[i]),c(DF$rank[i],DF$rank[DF$maxNode[i]==DF$node1]),lty=2)
-  #     
-  #   }
-  #   
-  #   axis(1,at = round(seq(min(DF$value),max(DF$value),length=6),2))
-  #   axis(2,at = seq_len(nrow(DF)),labels = DF$node1,las=1)
-  #   
-  #   
-  #   Quantiles[Quantiles$node1 %in% c("V5","V6") & Quantiles$node2  %in% c("V5","V6"),]
-  #   
-  #   # ggplot(DF, aes(x=value,y=rank)) + 
-  #   #   geom_line() + geom_point() + 
-  #   #   theme_bw() + 
-  #   #   xlab(measure) + 
-  #   #   geom_line(aes(x=))
-  #   
-  #   invisible(NULL)
-}
+# 
+# overlap <- function(x,measure = c("strength","closeness","betweenness"),
+#                     order = c("value","order")){
+#   
+#   order <- match.arg(order)
+#   measure <- match.arg(measure)
+#   
+#   cent <- x$bootTable %>% filter(type %in% measure) %>% dplyr::select(name,node1,value,type)
+#   fullTable <- expand.grid(name = unique(cent$name),node1=unique(cent$node1),node2=unique(cent$node1),type = unique(cent$type),
+#                            stringsAsFactors = FALSE) 
+#   
+#   Quantiles <- fullTable %>% 
+#     left_join(dplyr::select(cent,name,node1,value1=value,type),by=c("name","node1","type")) %>% 
+#     left_join(dplyr::select(cent,name,node2=node1,value2=value,type),by=c("name","node2","type"))  %>%
+#     group_by(node1,node2,type) %>%
+#     summarize(lower = quantile(value2-value1,0.025),upper = quantile(value2-value1,0.975)) %>%
+#     mutate(contain0 = 0 >= lower & 0 <= upper)
+#   
+#   #bootmean:
+#   bootMeans <- x$bootTable %>% filter(type %in% measure) %>%
+#     group_by(node1,type) %>% summarize(mean = mean(value,na.rm=TRUE))
+#   
+#   sample <- x$sampleTable %>% filter(type %in% measure) %>% dplyr::select(node1,value,type) %>% 
+#     left_join(bootMeans,by=c("node1","type"))
+#   
+#   # Now for every node: minimal node equal to....
+#   DF <- Quantiles %>% left_join(dplyr::select(sample,node2=node1,value,type), by = c("node2","type")) %>%
+#     group_by(node1,type) %>% 
+#     summarize(
+#       minNode = node2[contain0][which.min(value[contain0])],
+#       maxNode = node2[contain0][which.max(value[contain0])]
+#     ) %>% left_join(sample,by=c("node1","type")) %>% ungroup %>%
+#     mutate(valueMin = value[match(minNode,node1)], 
+#            valueMax = value[match(maxNode,node1)],
+#            rank = order(order(value,mean))) %>% arrange(rank)
+#   
+#   if (order == "value"){
+#     levels <- DF$node1[order(DF$value)]  
+#   } else  if (order == "order"){
+#     levels <- x$sample$labels
+#   }
+#   
+#   Quantiles$node1 <- factor(Quantiles$node1,levels=levels)
+#   Quantiles$node2 <- factor(Quantiles$node2,levels=levels)
+#   Quantiles$fill <- ifelse(Quantiles$node1 == Quantiles$node2, "same",
+#                            ifelse(Quantiles$contain0,"nonsig","sig"))
+#   DF$node2 <- DF$node1
+#   DF$node1 <- factor(DF$node1,levels=levels)
+#   DF$node2 <- factor(DF$node2,levels=levels)
+#   DF$label <- as.character(round(DF$value,2))
+#   DF$fill <- "same"
+#   
+#   lab <- measure
+#   substr(lab,1,1) <- toupper(substr(lab,1,1))
+#   
+#   g <- ggplot(Quantiles,aes(x=node1,y=node2,fill=fill)) + 
+#     geom_tile(colour = 'white') + xlab("") + ylab("") + 
+#     scale_fill_manual(values = c("same" = "white","nonsig" = "lightgray","sig" = "black")) + 
+#     geom_text(data=DF,aes(label = label))+ theme(legend.position="none") + 
+#     ggtitle(lab)
+#   
+#   base_size <- 9
+#   g <- g + theme_grey(base_size = base_size) + labs(x = "",
+#                                                     y = "") + scale_x_discrete(expand = c(0, 0)) +
+#     scale_y_discrete(expand = c(0, 0)) + theme(legend.position = "none",
+#                                                axis.ticks = element_blank(), axis.text.x = element_text(size = base_size *
+#                                                                                                           0.8, angle = 330, hjust = 0, colour = "grey50"))
+#   
+#   return(g)
+#   #   
+#   #   plot(DF$value,DF$rank,type="o",xaxt="n",yaxt="n",bty="n",
+#   #        xlab = measure, ylab = "node", pch = 17, cex = 2)
+#   #   
+#   #   for (i in seq_len(nrow(DF))){
+#   #     
+#   #     lines(c(DF$valueMin[i],DF$valueMax[i]),c(DF$rank[i],DF$rank[i]),lty=2)
+#   #     lines(c(DF$valueMin[i],DF$valueMin[i]),c(DF$rank[i],DF$rank[DF$minNode[i]==DF$node1]),lty=2)
+#   #     lines(c(DF$valueMax[i],DF$valueMax[i]),c(DF$rank[i],DF$rank[DF$maxNode[i]==DF$node1]),lty=2)
+#   #     
+#   #   }
+#   #   
+#   #   axis(1,at = round(seq(min(DF$value),max(DF$value),length=6),2))
+#   #   axis(2,at = seq_len(nrow(DF)),labels = DF$node1,las=1)
+#   #   
+#   #   
+#   #   Quantiles[Quantiles$node1 %in% c("V5","V6") & Quantiles$node2  %in% c("V5","V6"),]
+#   #   
+#   #   # ggplot(DF, aes(x=value,y=rank)) + 
+#   #   #   geom_line() + geom_point() + 
+#   #   #   theme_bw() + 
+#   #   #   xlab(measure) + 
+#   #   #   geom_line(aes(x=))
+#   #   
+#   #   invisible(NULL)
+# }
---
+++ R/checkInput.R
@@ -0,0 +1,170 @@
+
+# Function that checks input and returns the functions:
+checkInput <- function(
+  default = c("none", "EBICglasso", "pcor","IsingFit","IsingLL", "huge","adalasso"),
+  prepFun, # Fun to produce the correlation or covariance matrix
+  prepArgs = list(), # list with arguments for the correlation function
+  estFun, # function that results in a network
+  estArgs, # arguments sent to the graph estimation function (if missing automatically sample size is included)
+  graphFun, # set to identity if missing
+  graphArgs, # Set to null if missing
+  intFun, # Set to null if missing
+  intArgs, # Set to null if missing
+  sampleSize
+){
+  if (default[[1]]=="glasso") default <- "EBICglasso"
+  default <- match.arg(default)
+  
+  ### DEFAULT OPTIONS ###
+  if ((default == "none")){
+    if (missing(prepFun) | missing(prepArgs) | missing(estFun) | missing(estArgs)){
+      stop("If 'default' is not set, 'prepFun', 'prepArgs', 'estFun' and 'estArgs' may not be missing.")
+    }
+  }
+  
+  if (!(default == "none")){
+    # prepFun:
+    if (missing(prepFun)){
+      prepFun <- switch(default,
+                        EBICglasso = qgraph::cor_auto,
+                        IsingFit = binarize,
+                        IsingLL = binarize,
+                        pcor = qgraph::cor_auto,
+                        huge = function(x)huge::huge.npn(na.omit(as.matrix(x)),verbose = FALSE),
+                        adalasso = identity
+      )
+      #       prepFun <- switch(default,
+      #                         EBICglasso = cor,
+      #                         IsingFit = binarize,
+      #                         pcor = cor
+      #       )      
+    }
+    
+    # prepArgs:
+    #     qgraphVersion <- packageDescription("qgraph")$Version
+    #     qgraphVersion <- as.numeric(strsplit(qgraphVersion,split="\\.|\\-")[[1]])
+    #     if (length(qgraphVersion)==1) qgraphVersion <- c(qgraphVersion,0)
+    #     if (length(qgraphVersion)==2) qgraphVersion <- c(qgraphVersion,0)
+    #     goodVersion <- 
+    #       (qgraphVersion[[1]] >= 1 & qgraphVersion[[2]] >= 3 & qgraphVersion[[3]] >= 1) | 
+    #       (qgraphVersion[[1]] >= 1 & qgraphVersion[[2]] > 3) | 
+    #       qgraphVersion[[1]] > 1
+    
+    
+    if (missing(prepArgs)){
+      prepArgs <- switch(default,
+                         EBICglasso = ifElse(identical(prepFun,qgraph::cor_auto),list(verbose=FALSE),
+                                             ifElse(identical(prepFun,cor),list(use = "pairwise.complete.obs"),list())),
+                         IsingFit = list(),
+                         pcor =  ifElse(identical(prepFun,qgraph::cor_auto),list(verbose=FALSE),
+                                        ifElse(identical(prepFun,cor),list(use = "pairwise.complete.obs"),list())),
+                         IsingLL = list(),
+                         huge = list(),
+                         adalasso = list()
+      )
+      
+      
+    }
+    
+    # estFun:
+    if (missing(estFun)){
+      estFun <- switch(default,
+                       EBICglasso = qgraph::EBICglasso,
+                       pcor = corpcor::cor2pcor,
+                       IsingFit = IsingFit::IsingFit,
+                       IsingLL = IsingSampler::EstimateIsing,
+                       huge = function(x)huge::huge.select(huge::huge(x,method = "glasso",verbose=FALSE), criterion = "ebic",verbose = FALSE),
+                       adalasso = parcor::adalasso.net
+      )
+    }
+    
+    # estArgs:
+    if (missing(estArgs)){
+      estArgs <- switch(default,
+                        EBICglasso = list(n = sampleSize, returnAllResults = TRUE),
+                        IsingFit = list(plot = FALSE, progress = FALSE),
+                        pcor = list(),
+                        IsingLL = list(method = "ll"),
+                        huge = list(),
+                        adalasso = list()
+      )
+    }
+    
+    # graphFun:
+    if (missing(graphFun)){
+      graphFun <- switch(default,
+                         EBICglasso = function(x)x[['optnet']],
+                         IsingFit = function(x)x[['weiadj']],
+                         pcor = function(x)as.matrix(Matrix::forceSymmetric(x)),
+                         IsingLL = function(x)x[['graph']],
+                         huge = function(x)as.matrix(qgraph::wi2net(as.matrix(x$opt.icov))),
+                         adalasso = function(x)as.matrix(Matrix::forceSymmetric(x$pcor.adalasso))
+      )
+    }
+    
+    # graphArgs:
+    if (missing(graphArgs)){
+      graphArgs <- switch(default,
+                          EBICglasso = list(),
+                          IsingFit = list(),
+                          pcor = list(),
+                          IsingLL = list(),
+                          huge = list(),
+                          adalasso = list()
+      )
+    }
+    
+    # intFun:
+    if (missing(intFun)){
+      intFun <- switch(default,
+                       EBICglasso = null,
+                       IsingFit = function(x)x[['thresholds']],
+                       pcor = null,
+                       IsingLL = function(x) x[['thresholds']],
+                       huge = null,
+                       adalasso = null
+      )
+    }
+    
+    
+  }
+  
+  if (missing(prepFun)){
+    prepFun <- identity
+  }
+  
+  if (missing(prepArgs)){
+    prepArgs <- list()
+  }
+  
+  if (missing(graphFun)){
+    graphFun <- identity
+  }
+  
+  if (missing(graphArgs)){
+    graphArgs <- list()
+  }
+  
+  if (missing(intFun)){
+    intFun <- null
+  }
+  
+  if (missing(intArgs)){
+    intArgs <- list()
+  }
+  
+  Args <- list(
+    data = data,
+    default = default,
+    prepFun = prepFun, # Fun to produce the correlation or covariance matrix
+    prepArgs = prepArgs, # list with arguments for the correlation function
+    estFun = estFun, # function that results in a network
+    estArgs = estArgs, # arguments sent to the graph estimation function (if missing automatically sample size is included)
+    graphFun = graphFun, # set to identity if missing
+    graphArgs = graphArgs, # Set to null if missing
+    intFun = intFun, # Set to null if missing
+    intArgs = intArgs
+  )
+  
+  return(Args)
+}
