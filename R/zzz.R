c7d93c3c805bb9265d67b9354241d836f36a68e0
Sacha Epskamp <mail@sachaepskamp.com>
2017-03-28 17:42:16 +0300

    Different startup message

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,8 @@
   version <- read.dcf(file=system.file("DESCRIPTION", package=pkgname),
                       fields="Version")
   packageStartupMessage("This is ",paste(pkgname, version))
-  packageStartupMessage(pkgname, " is BETA software! Please report any bugs.")
+  # packageStartupMessage(pkgname, " is BETA software! Please report any bugs.")
+  packageStartupMessage("For questions and issues, please see github.com/SachaEpskamp/bootnet.")
 }
 
 # Please do not look further down this script.
