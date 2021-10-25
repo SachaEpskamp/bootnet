4593dafc92024ae7f93bad9a390895ccaf3938e7
Sacha Epskamp <sacha.epskamp@gmail.com>
2021-10-25 16:21:37 +0200

    dplyr fixes and subset bug

--- R/bootTable.R
+++ R/bootTable.R
@@ -69,7 +69,7 @@ statTable <- function(x,
   Wmat <- qgraph::getWmat(x)
   
   if ("edge" %in% statistics){
-    tables$edges <- dplyr::tbl_df(data.frame(
+    tables$edges <- tibble::as_tibble(data.frame(
       name = name,
       type = "edge",
       node1 = x[['labels']][ind[,1]],
@@ -81,7 +81,7 @@ statTable <- function(x,
   
   
   if ("length" %in% statistics){ 
-    tables$length <- dplyr::tbl_df(data.frame(
+    tables$length <- tibble::as_tibble(data.frame(
       name = name,
       type = "length",
       node1 = x[['labels']][ind[,1]],
@@ -93,7 +93,7 @@ statTable <- function(x,
   
   # Intercepts:
   if (!is.null(x[['intercepts']])){
-    tables$intercepts <- dplyr::tbl_df(data.frame(
+    tables$intercepts <- tibble::as_tibble(data.frame(
       name = name,
       type = "intercept",
       node1 = x[['labels']],
@@ -167,7 +167,7 @@ statTable <- function(x,
     
     # strength:
     if ("strength" %in% statistics & !directed){
-      tables$strength <- dplyr::tbl_df(data.frame(
+      tables$strength <- tibble::as_tibble(data.frame(
         name = name,
         type = "strength",
         node1 = x[['labels']],
@@ -178,7 +178,7 @@ statTable <- function(x,
     }
     
     if ("outStrength" %in% statistics && directed){
-      tables$outStrength <- dplyr::tbl_df(data.frame(
+      tables$outStrength <- tibble::as_tibble(data.frame(
         name = name,
         type = "outStrength",
         node1 = x[['labels']],
@@ -189,7 +189,7 @@ statTable <- function(x,
     }
     if ("inStrength" %in% statistics && directed){
       
-      tables$inStrength <- dplyr::tbl_df(data.frame(
+      tables$inStrength <- tibble::as_tibble(data.frame(
         name = name,
         type = "inStrength",
         node1 = x[['labels']],
@@ -201,7 +201,7 @@ statTable <- function(x,
     
     # closeness:
     if ("closeness" %in% statistics){
-      tables$closeness <- dplyr::tbl_df(data.frame(
+      tables$closeness <- tibble::as_tibble(data.frame(
         name = name,
         type = "closeness",
         node1 = x[['labels']],
@@ -214,7 +214,7 @@ statTable <- function(x,
     
     # betweenness:
     if ("betweenness" %in% statistics){
-      tables$betweenness <- dplyr::tbl_df(data.frame(
+      tables$betweenness <- tibble::as_tibble(data.frame(
         name = name,
         type = "betweenness",
         node1 = x[['labels']],
@@ -225,7 +225,7 @@ statTable <- function(x,
     }
     
     if ("distance" %in% statistics){
-      tables$sp <- dplyr::tbl_df(data.frame(
+      tables$sp <- tibble::as_tibble(data.frame(
         name = name,
         type = "distance",
         node1 = x[['labels']][ind[,1]],
@@ -236,7 +236,7 @@ statTable <- function(x,
     }
     
     if ("expectedInfluence" %in% statistics && !directed){
-      tables$expectedInfluence <- dplyr::tbl_df(data.frame(
+      tables$expectedInfluence <- tibble::as_tibble(data.frame(
         name = name,
         type = "expectedInfluence",
         node1 = x[['labels']],
@@ -249,7 +249,7 @@ statTable <- function(x,
     # randomized shortest paths betweenness centrality:
     if ("rspbc" %in% statistics){
       tryrspbc <- try({
-        tables$rspbc <- dplyr::tbl_df(data.frame(
+        tables$rspbc <- tibble::as_tibble(data.frame(
           name = name,
           type = "rspbc",
           node1 = x[['labels']],
@@ -260,7 +260,7 @@ statTable <- function(x,
       })
       
       if (is(tryrspbc,"try-error")){
-        tables$rspbc <- dplyr::tbl_df(data.frame(
+        tables$rspbc <- tibble::as_tibble(data.frame(
           name = name,
           type = "rspbc",
           node1 = x[['labels']],
@@ -275,7 +275,7 @@ statTable <- function(x,
     if ("hybrid" %in% statistics){
       
       tryhybrid <- try({
-        tables$hybrid <- dplyr::tbl_df(data.frame(
+        tables$hybrid <- tibble::as_tibble(data.frame(
           name = name,
           type = "hybrid",
           node1 = x[['labels']],
@@ -286,7 +286,7 @@ statTable <- function(x,
       })
       
       if (is(tryhybrid,"try-error")){
-        tables$rspbc <- dplyr::tbl_df(data.frame(
+        tables$rspbc <- tibble::as_tibble(data.frame(
           name = name,
           type = "hybrid",
           node1 = x[['labels']],
@@ -302,7 +302,7 @@ statTable <- function(x,
     if ("eigenvector" %in% statistics){
       
       tryeigenvector <- try({
-        tables$eigenvector <- dplyr::tbl_df(data.frame(
+        tables$eigenvector <- tibble::as_tibble(data.frame(
           name = name,
           type = "eigenvector",
           node1 = x[['labels']],
@@ -313,7 +313,7 @@ statTable <- function(x,
       })
       
       if (is(tryeigenvector,"try-error")){
-        tables$eigenvector <- dplyr::tbl_df(data.frame(
+        tables$eigenvector <- tibble::as_tibble(data.frame(
           name = name,
           type = "eigenvector",
           node1 = x[['labels']],
@@ -327,7 +327,7 @@ statTable <- function(x,
     
     if ("outExpectedInfluence" %in% statistics && directed){
       
-      tables$outExpectedInfluence <- dplyr::tbl_df(data.frame(
+      tables$outExpectedInfluence <- tibble::as_tibble(data.frame(
         name = name,
         type = "outExpectedInfluence",
         node1 = x[['labels']],
@@ -338,7 +338,7 @@ statTable <- function(x,
     }
     if ("inExpectedInfluence" %in% statistics && directed){
       
-      tables$inExpectedInfluence <- dplyr::tbl_df(data.frame(
+      tables$inExpectedInfluence <- tibble::as_tibble(data.frame(
         name = name,
         type = "inExpectedInfluence",
         node1 = x[['labels']],
@@ -351,7 +351,7 @@ statTable <- function(x,
     
     # bridgeStrength:
     if ("bridgeStrength" %in% statistics){
-      tables$bridgeStrength <- dplyr::tbl_df(data.frame(
+      tables$bridgeStrength <- tibble::as_tibble(data.frame(
         name = name,
         type = "bridgeStrength",
         node1 = bridgeCentralityNames,
@@ -363,7 +363,7 @@ statTable <- function(x,
     
     # bridgeCloseness:
     if ("bridgeCloseness" %in% statistics){
-      tables$bridgeCloseness <- dplyr::tbl_df(data.frame(
+      tables$bridgeCloseness <- tibble::as_tibble(data.frame(
         name = name,
         type = "bridgeCloseness",
         node1 = bridgeCentralityNames,
@@ -375,7 +375,7 @@ statTable <- function(x,
     
     # bridgeBetweenness:
     if ("bridgeBetweenness" %in% statistics){
-      tables$bridgeBetweenness <- dplyr::tbl_df(data.frame(
+      tables$bridgeBetweenness <- tibble::as_tibble(data.frame(
         name = name,
         type = "bridgeBetweenness",
         node1 = bridgeCentralityNames,
@@ -387,7 +387,7 @@ statTable <- function(x,
     
     # bridgeExpectedInfluence:
     if ("bridgeExpectedInfluence" %in% statistics){
-      tables$bridgeExpectedInfluence <- dplyr::tbl_df(data.frame(
+      tables$bridgeExpectedInfluence <- tibble::as_tibble(data.frame(
         name = name,
         type = "bridgeExpectedInfluence",
         node1 = bridgeCentralityNames,
@@ -411,7 +411,7 @@ statTable <- function(x,
   tab$nPerson <- x$nPerson
   
   # Compute rank:
-  tab <- tab %>% group_by(type) %>%
+  tab <- tab %>% group_by(.data[['type']]) %>%
     mutate(rank_avg = rank(value,ties.method = "average"),
            rank_min = rank(value,ties.method = "min"),
            rank_max = rank(value,ties.method = "max"))
--- R/summaryMethod.R
+++ R/summaryMethod.R
@@ -69,49 +69,50 @@ summary.bootnet <- function(
 
       N <- object$sampleSize
       tab <- object$bootTable %>% 
-        dplyr::filter_(~type %in% statistics) %>%
-        dplyr::left_join(object$sampleTable %>% dplyr::select_(~type,~id,~node1,~node2,sample = ~value), by=c("id","type","node1","node2")) %>%
-        dplyr::mutate_(PS = ~N*sample - (N-1)*value) %>%
-        dplyr::group_by_(~type, ~node1, ~node2, ~id) %>%
-        dplyr::summarize_(
-          mean = ~mean(value),
-          sample = ~mean(PS),
-          var = ~(1/(N-1)) * sum((PS - value)^2),
-          CIlower = ~sample - 2 * sqrt(var/N),
-          CIupper = ~sample + 2 * sqrt(var/N)
+        dplyr::filter(.data[['type']] %in% statistics) %>%
+        dplyr::left_join(object$sampleTable %>% dplyr::select(.data[['type']],.data[['id']],.data[['node1']],.data[['node2']],sample = .data[['value']]), by=c("id","type","node1","node2")) %>%
+        dplyr::mutate(PS = N*.data[['sample']] - (N-1)*.data[['value']]) %>%
+        dplyr::group_by(.data[['type']], .data[['node1']], .data[['node2']], .data[['id']]) %>%
+        dplyr::summarize(
+          mean = mean(.data[['value']]),
+          sample = mean(.data[['PS']]),
+          var = (1/(N-1)) * sum((.data[['PS']] - .data[['value']])^2),
+          CIlower = .data[['sample']] - 2 * sqrt(.data[['var']]/N),
+          CIupper = .data[['sample']] + 2 * sqrt(.data[['var']]/N)
         )%>%
-        dplyr::select_(~type, ~id, ~node1, ~node2, ~sample, ~mean, ~CIlower, ~CIupper)
+        dplyr::select(.data[['type']], .data[['id']], .data[['node1']], .data[['node2']], .data[['sample']], .data[['mean']], .data[['CIlower']], .data[['CIupper']])
       
     } else {
+
       tab <- object$bootTable %>% 
-        dplyr::filter_(~type %in% statistics) %>%
-        dplyr::group_by_(~type, ~node1, ~node2, ~id) %>%
-        dplyr::summarize_(
-          mean = ~mean(value,na.rm=TRUE),
-          var = ~var(value,na.rm=TRUE),
-          sd = ~sd(value,na.rm=TRUE),
-          prop0 = ~mean(abs(value) < tol) %>% naTo0,
+        dplyr::filter(.data[['type']] %in% statistics) %>%
+        dplyr::group_by(.data[['type']], .data[['node1']], .data[['node2']], .data[['id']]) %>%
+        dplyr::summarize(
+          mean = mean(.data[['value']],na.rm=TRUE),
+          var = var(.data[['value']],na.rm=TRUE),
+          sd = sd(.data[['value']],na.rm=TRUE),
+          prop0 = mean(abs(.data[['value']]) < tol) %>% naTo0,
           # q1 = ~quantile(value,1/100, na.rm = TRUE),
-          q2.5 = ~quantile(value_min, 2.5/100, na.rm = TRUE, type = 6) %>% naTo0,
+          q2.5 = quantile(.data[['value_min']], 2.5/100, na.rm = TRUE, type = 6) %>% naTo0,
           #                 q5 = ~quantile(value, 5/100, na.rm = TRUE),
           #                 q25 = ~quantile(value, 25/100, na.rm = TRUE),
           #                 q50 = ~quantile(value, 50/100, na.rm = TRUE),
           #                 q75 = ~quantile(value, 75/100, na.rm = TRUE),
           #                 q95 = ~quantile(value, 95/100, na.rm = TRUE),
-          q97.5 = ~quantile(value_max, 97.5/100, na.rm = TRUE, type = 6) %>% naTo0,
-          q2.5_non0 = ~quantile(value_min[!abs(value_min) < tol], 2.5/100, na.rm = TRUE, type = 6) %>% naTo0,
-          mean_non0 = ~mean(value[!abs(value) < tol],  na.rm = TRUE) %>% naTo0,
-          q97.5_non0 = ~quantile(value_max[!abs(value_max) < tol], 97.5/100, na.rm = TRUE, type = 6) %>% naTo0,
-          var_non0 = ~var(value[!abs(value) < tol],na.rm=TRUE) %>% naTo0,
-          sd_non0 = ~sd(value[!abs(value) < tol],na.rm=TRUE) %>% naTo0
+          q97.5 = quantile(.data[['value_max']], 97.5/100, na.rm = TRUE, type = 6) %>% naTo0,
+          q2.5_non0 = quantile(.data[['value_min']][!abs(.data[['value_min']]) < tol], 2.5/100, na.rm = TRUE, type = 6) %>% naTo0,
+          mean_non0 = mean(.data[['value']][!abs(.data[['value']]) < tol],  na.rm = TRUE) %>% naTo0,
+          q97.5_non0 = quantile(.data[['value_max']][!abs(.data[['value_max']]) < tol], 97.5/100, na.rm = TRUE, type = 6) %>% naTo0,
+          var_non0 = var(.data[['value']][!abs(.data[['value']]) < tol],na.rm=TRUE) %>% naTo0,
+          sd_non0 = sd(.data[['value']][!abs(.data[['value']]) < tol],na.rm=TRUE) %>% naTo0
           # q99 = ~quantile(value, 99/100, na.rm = TRUE)
         ) %>%
-        dplyr::left_join(object$sampleTable %>% dplyr::select_(~type,~id,~node1,~node2,sample = ~value), by=c("id","type","node1","node2"))   %>%
-        dplyr::mutate_(
-          CIlower = ~sample-2*sd, CIupper = ~sample + 2*sd,
-          CIlower_non0 = ~mean_non0 - 2*sd_non0, CIupper_non0 = ~mean_non0 + 2*sd_non0) %>%
-        dplyr::select_(~type, ~id, ~node1, ~node2, ~sample, ~mean, ~sd, ~CIlower, ~CIupper,
-                       ~q2.5, ~q97.5, ~q2.5_non0,  ~mean_non0,  ~q97.5_non0,  ~var_non0,  ~sd_non0, ~prop0)
+        dplyr::left_join(object$sampleTable %>% dplyr::select(.data[['type']],.data[['id']],.data[['node1']],.data[['node2']],sample = .data[['value']]), by=c("id","type","node1","node2"))   %>%
+        dplyr::mutate(
+          CIlower = .data[['sample']]-2*.data[['sd']], CIupper = .data[['sample']] + 2*.data[['sd']],
+          CIlower_non0 = .data[['mean_non0']] - 2*.data[['sd_non0']], CIupper_non0 = .data[['mean_non0']] + 2*.data[['sd_non0']]) %>%
+        dplyr::select(.data[['type']], .data[['id']], .data[['node1']], .data[['node2']], .data[['sample']], .data[['mean']], .data[['sd']], .data[['CIlower']], .data[['CIupper']],
+                       .data[['q2.5']], .data[['q97.5']], .data[['q2.5_non0']],  .data[['mean_non0']],  .data[['q97.5_non0']],  .data[['var_non0']],  .data[['sd_non0']], .data[['prop0']])
     }
     
     
@@ -120,54 +121,54 @@ summary.bootnet <- function(
 
     # Nodewise
     tab <- object$bootTable %>% 
-      dplyr::filter_(~type %in% statistics) %>% 
-      dplyr::left_join(object$sampleTable %>% dplyr::select_(~type,~id,~node1,~node2,sample = ~value), by=c("id","type","node1","node2"))
+      dplyr::filter(.data[['type']] %in% statistics) %>% 
+      dplyr::left_join(object$sampleTable %>% dplyr::select(.data[['type']],.data[['id']],.data[['node1']],.data[['node2']],sample = .data[['value']]), by=c("id","type","node1","node2"))
     
     if (perNode){
-      tab <- tab %>% group_by_(~id, ~type, ~nNode, ~nPerson)  %>%
-        dplyr::summarize_(
-          mean = ~mean(value,na.rm=TRUE),
-          var = ~var(value,na.rm=TRUE),
-          sd = ~sd(value,na.rm=TRUE),
-          q1 = ~quantile(value,1/100, na.rm = TRUE, type = 6) %>% naTo0,
-          q2.5 = ~quantile(value, 2.5/100, na.rm = TRUE, type = 6) %>% naTo0,
-          q5 = ~quantile(value, 5/100, na.rm = TRUE, type = 6) %>% naTo0,
-          q25 = ~quantile(value, 25/100, na.rm = TRUE, type = 6) %>% naTo0,
-          q50 = ~quantile(value, 50/100, na.rm = TRUE, type = 6) %>% naTo0,
-          q75 = ~quantile(value, 75/100, na.rm = TRUE, type = 6) %>% naTo0,
-          q95 = ~quantile(value, 95/100, na.rm = TRUE, type = 6) %>% naTo0,
-          q97.5 = ~quantile(value, 97.5/100, na.rm = TRUE, type = 6) %>% naTo0,
-          q99 = ~quantile(value, 99/100, na.rm = TRUE, type = 6) %>% naTo0,
-          prop0 = ~mean(abs(value) < tol),
-          q2.5_non0 = ~quantile(value[!abs(value) < tol], 2.5/100, na.rm = TRUE, type = 6) %>% naTo0,
-          mean_non0 = ~mean(value[!abs(value) < tol],  na.rm = TRUE) %>% naTo0,
-          q97.5_non0 = ~quantile(value[!abs(value) < tol], 97.5/100, na.rm = TRUE, type = 6) %>% naTo0,
-          var_non0 = ~var(value[!abs(value) < tol],na.rm=TRUE) %>% naTo0,
-          sd_non0 = ~sd(value[!abs(value) < tol],na.rm=TRUE) %>% naTo0
-        ) %>% mutate_(
-          CIlower = ~mean - 2*sd, CIupper = ~mean + 2*sd,
-          CIlower_non0 = ~mean_non0 - 2*sd_non0, CIupper_non0 = ~mean_non0 + 2*sd_non0
-          ) %>% arrange_(~nNode,~nPerson)
+      tab <- tab %>% group_by(.data[['id']], .data[['type']], .data[['nNode']], .data[['nPerson']])  %>%
+        dplyr::summarize(
+          mean = mean(.data[['value']],na.rm=TRUE),
+          var = var(.data[['value']],na.rm=TRUE),
+          sd = sd(.data[['value']],na.rm=TRUE),
+          q1 = quantile(.data[['value']],1/100, na.rm = TRUE, type = 6) %>% naTo0,
+          q2.5 = quantile(.data[['value']], 2.5/100, na.rm = TRUE, type = 6) %>% naTo0,
+          q5 = quantile(.data[['value']], 5/100, na.rm = TRUE, type = 6) %>% naTo0,
+          q25 = quantile(.data[['value']], 25/100, na.rm = TRUE, type = 6) %>% naTo0,
+          q50 = quantile(.data[['value']], 50/100, na.rm = TRUE, type = 6) %>% naTo0,
+          q75 = quantile(.data[['value']], 75/100, na.rm = TRUE, type = 6) %>% naTo0,
+          q95 = quantile(.data[['value']], 95/100, na.rm = TRUE, type = 6) %>% naTo0,
+          q97.5 = quantile(.data[['value']], 97.5/100, na.rm = TRUE, type = 6) %>% naTo0,
+          q99 = quantile(.data[['value']], 99/100, na.rm = TRUE, type = 6) %>% naTo0,
+          prop0 = mean(abs(.data[['value']]) < tol),
+          q2.5_non0 = quantile(.data[['value']][!abs(.data[['value']]) < tol], 2.5/100, na.rm = TRUE, type = 6) %>% naTo0,
+          mean_non0 = mean(.data[['value']][!abs(.data[['value']]) < tol],  na.rm = TRUE) %>% naTo0,
+          q97.5_non0 = quantile(.data[['value']][!abs(.data[['value']]) < tol], 97.5/100, na.rm = TRUE, type = 6) %>% naTo0,
+          var_non0 = var(.data[['value']][!abs(.data[['value']]) < tol],na.rm=TRUE) %>% naTo0,
+          sd_non0 = sd(.data[['value']][!abs(.data[['value']]) < tol],na.rm=TRUE) %>% naTo0
+        ) %>% mutate(
+          CIlower = .data[['mean']] - 2*.data[['sd']], CIupper = .data[['mean']] + 2*.data[['sd']],
+          CIlower_non0 = .data[['mean_non0']] - 2*.data[['sd_non0']], CIupper_non0 = .data[['mean_non0']] + 2*.data[['sd_non0']]
+          ) %>% arrange(.data[['nNode']],.data[['nPerson']])
       
     } else {
 
-      tab <- tab %>% group_by_(~name, ~type, ~nNode, ~nPerson)  %>%
-        summarize_(cor = ~ suppressWarnings(cor(value,sample, use = "pairwise.complete.obs"))) %>%
-        dplyr::group_by_(~nNode, ~nPerson, ~type) %>%
-        dplyr::summarize_(
-          mean = ~mean(cor,na.rm=TRUE),
-          var = ~var(cor,na.rm=TRUE),
-          sd = ~sd(cor,na.rm=TRUE),
-          q1 = ~quantile(cor,1/100, na.rm = TRUE, type = 6) %>% naTo0,
-          q2.5 = ~quantile(cor, 2.5/100, na.rm = TRUE, type = 6) %>% naTo0,
-          q5 = ~quantile(cor, 5/100, na.rm = TRUE, type = 6) %>% naTo0,
-          q25 = ~quantile(cor, 25/100, na.rm = TRUE, type = 6) %>% naTo0,
-          q50 = ~quantile(cor, 50/100, na.rm = TRUE, type = 6) %>% naTo0,
-          q75 = ~quantile(cor, 75/100, na.rm = TRUE, type = 6) %>% naTo0,
-          q95 = ~quantile(cor, 95/100, na.rm = TRUE, type = 6) %>% naTo0,
-          q97.5 = ~quantile(cor, 97.5/100, na.rm = TRUE, type = 6) %>% naTo0,
-          q99 = ~quantile(cor, 99/100, na.rm = TRUE, type = 6) %>% naTo0
-        ) %>% arrange_(~nNode, ~nPerson)
+      tab <- tab %>% group_by(.data[['name']], .data[['type']], .data[['nNode']], .data[['nPerson']])  %>%
+        summarize(cor = suppressWarnings(cor(.data[['value']],sample, use = "pairwise.complete.obs"))) %>%
+        dplyr::group_by(.data[['nNode']], .data[['nPerson']], .data[['type']]) %>%
+        dplyr::summarize(
+          mean = mean(.data[['cor']],na.rm=TRUE),
+          var = var(.data[['cor']],na.rm=TRUE),
+          sd = sd(.data[['cor']],na.rm=TRUE),
+          q1 = quantile(.data[['cor']],1/100, na.rm = TRUE, type = 6) %>% naTo0,
+          q2.5 = quantile(.data[['cor']], 2.5/100, na.rm = TRUE, type = 6) %>% naTo0,
+          q5 = quantile(.data[['cor']], 5/100, na.rm = TRUE, type = 6) %>% naTo0,
+          q25 = quantile(.data[['cor']], 25/100, na.rm = TRUE, type = 6) %>% naTo0,
+          q50 = quantile(.data[['cor']], 50/100, na.rm = TRUE, type = 6) %>% naTo0,
+          q75 = quantile(.data[['cor']], 75/100, na.rm = TRUE, type = 6) %>% naTo0,
+          q95 = quantile(.data[['cor']], 95/100, na.rm = TRUE, type = 6) %>% naTo0,
+          q97.5 = quantile(.data[['cor']], 97.5/100, na.rm = TRUE, type = 6) %>% naTo0,
+          q99 = quantile(.data[['cor']], 99/100, na.rm = TRUE, type = 6) %>% naTo0
+        ) %>% arrange(.data[['nNode']], .data[['nPerson']])
       
     }
   }
